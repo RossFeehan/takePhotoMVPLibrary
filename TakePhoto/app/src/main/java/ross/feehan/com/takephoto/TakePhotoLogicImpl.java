3fe4fca8361a467d420d9910fe1d5d01be6f1778
rossfeehan <thafirstboss@yahoo.co.uk>
2016-03-13 20:17:38 +0000

    Created TakePhotoLogic class that implements TakePhotoLogicInterface, also set up test class for TakePhotoLogicImpl

---
+++ TakePhoto/app/src/main/java/ross/feehan/com/takephoto/TakePhotoLogicImpl.java
@@ -0,0 +1,24 @@
+package ross.feehan.com.takephoto; /*
+ * Created by Ross Feehan on 13/03/2016.
+ *
+ * Code Version Created in -
+ *
+ * Ross Feehan on 13/03/2016
+ * 
+ * Copyright Fiat Accompli
+ */
+
+public class TakePhotoLogicImpl implements TakePhotoLogicInterface {
+
+    //Interface methods
+    //TakePhotoLogicInterface methods
+    @Override
+    public void onTakePhotoBtnClicked() {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode) {
+
+    }
+}
---
+++ TakePhoto/app/src/test/java/ross/feehan/com/takephoto/TakePhotoLogicImplTest.java
@@ -0,0 +1,16 @@
+package ross.feehan.com.takephoto;
+
+import junit.framework.TestCase;
+
+/*
+* Created by Ross Feehan on 13/03/2016.
+*
+* Code Version Created in -
+*
+* Ross Feehan on 13/03/2016
+* 
+* Copyright Fiat Accompli
+*/
+public class TakePhotoLogicImplTest extends TestCase {
+
+}
