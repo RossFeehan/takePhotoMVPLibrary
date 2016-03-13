4641505b9da679d8df5886406a9a2296d696d233
rossfeehan <thafirstboss@yahoo.co.uk>
2016-03-13 20:03:45 +0000

    Created class that creates file names for the pictures to be taken, also sets up the folder where the pictures are to be stored

--- TakePhoto/app/src/main/java/ross/feehan/com/takephoto/MainActivity.java
+++ TakePhoto/app/src/main/java/ross/feehan/com/takephoto/MainActivity.java
@@ -1,13 +1,11 @@
 package ross.feehan.com.takephoto;
+/*
+ * Created by Ross Feehan on 13/03/2016.
+ */
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ TakePhoto/app/src/main/java/ross/feehan/com/takephoto/PhotoFileUtils.java
@@ -0,0 +1,47 @@
+package ross.feehan.com.takephoto;
+/*
+ * Created by Ross Feehan on 13/03/2016.
+ */
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ */
+public class PhotoFileUtils {
+
+    /*Method to create a file to send to the default camera so it knows where to
+		*save the created image
+		*/
+    public static File createFileForDefaultCamera() {
+        // Location where the photos will be stored. The created images will be shared
+        // between applications and persist after app has been uninstalled.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory() + "/DCIM/Camera");
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("CreateFile", "Failed to create photo gallery directory");
+                return null;
+            }
+        }
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File image = null;
+        try {
+            image = File.createTempFile(imageFileName, ".jpg", mediaStorageDir);
+        } catch (IOException e) {
+            Log.d("MainActivity", "Failed to create photo file");
+            e.printStackTrace();
+        }
+        return image;
+    }
+}
