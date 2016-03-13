68ee242bcf4f35c1cbf449e78c81948b4bc2505c
rossfeehan <thafirstboss@yahoo.co.uk>
2016-03-13 21:57:55 +0000

    call logic to take a photo when FAB is clicked and display the image in the imageview after it has been taken successfully

--- TakePhoto/app/src/main/java/ross/feehan/com/takephoto/TakePhoto/TakePhotoActivity.java
+++ TakePhoto/app/src/main/java/ross/feehan/com/takephoto/TakePhoto/TakePhotoActivity.java
@@ -3,13 +3,21 @@ package ross.feehan.com.takephoto.TakePhoto;
  * Created by Ross Feehan on 13/03/2016.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ross.feehan.com.takephoto.R;
 import ross.feehan.com.takephoto.Utils.TakePhotoApplication;
 
@@ -17,6 +25,8 @@ import ross.feehan.com.takephoto.Utils.TakePhotoApplication;
 public class TakePhotoActivity extends AppCompatActivity implements TakePhotoViewInterface {
 
     @Inject TakePhotoLogicInterface logic;
+
+    @Bind(R.id.photoPreview) ImageView photoPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +37,29 @@ public class TakePhotoActivity extends AppCompatActivity implements TakePhotoVie
 
     private void initUI(){
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     private void initDependencies() {
         ((TakePhotoApplication)getApplication()).getObjectGraph().plus(new TakePhotoDIModule(this, this)).inject(this);
     }
 
+    //CLASS METHODS
+    @OnClick(R.id.takePictureFAB)
+    void onTakePictureBtnClicked(){
+        logic.onTakePhotoBtnClicked();
+    }
+
     //INTERFACE METHODS
     //TakePhotoViewInterface METHODS
     @Override
     public void displayTakenPhoto(File imageFile) {
+        Picasso.with(this).load(imageFile).into(photoPreview);
+    }
 
+    //ACTIVITY LIFECYCLE METHODS
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        logic.onActivityResult(requestCode, resultCode);
     }
 }
